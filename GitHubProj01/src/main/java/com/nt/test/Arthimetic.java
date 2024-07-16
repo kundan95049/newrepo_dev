2f20e47b688940fe3bebd7dfeb4918ea1d3e0bd7
KUNDAN KUMAR <KUNDAN KUMAR@Kundan>
2024-07-16 21:53:56 +0530

    commit

---
+++ GitHubProj01/src/main/java/com/nt/test/Arthimetic.java
@@ -0,0 +1,8 @@
+package com.nt.test;
+
+public class Arthimetic {
+	public int sum(int x, int y)
+	{
+	return x+y;
+	}
+}
---
+++ GitHubProj01/src/test/java/com/nt/test/AppTest.java
@@ -0,0 +1,18 @@
+package com.nt.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+	 @Test
+	  public void testWithPositive()
+	  {
+		  Arthimetic ar=new Arthimetic();
+		  int actual=30;
+		  int expected=ar.sum(10, 20);
+		  assertEquals(actual,expected); 
+	  }
+}
+
